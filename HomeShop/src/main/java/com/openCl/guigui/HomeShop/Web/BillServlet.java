63ee382bb5d3938f02b937456920df546f658d84
guillaume <bague.guillaume@live.fr>
2018-10-19 12:00:14 +0200

    Show bill

--- HomeShop/src/main/java/com/openCl/guigui/HomeShop/Web/BillServlet.java
+++ HomeShop/src/main/java/com/openCl/guigui/HomeShop/Web/BillServlet.java
@@ -1,16 +1,13 @@
 package com.openCl.guigui.HomeShop.Web;
 
-import com.openCl.guigui.HomeShop.Fridge;
-import com.openCl.guigui.HomeShop.Product;
-import com.openCl.guigui.HomeShop.Television;
+import com.openCl.guigui.HomeShop.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class BillServlet extends HttpServlet {
 
@@ -33,7 +30,7 @@ public class BillServlet extends HttpServlet {
         if (req.getQueryString() == null)
             displayForm(resp);
         else
-            displayBill();
+            displayBill(req, resp);
     }
 
     private void displayForm(HttpServletResponse resp) throws IOException{
@@ -59,7 +56,59 @@ public class BillServlet extends HttpServlet {
         resp.getWriter().println(form);
     }
 
-    private void displayBill(){
+    private void displayBill(HttpServletRequest req, HttpServletResponse resp) {
+        Map<String, String> params = splitParameters(req.getQueryString());
+        Client customer = new Client(params.get("fullname"), params.get("address"));
+        Delivery delivery = null;
+        switch (params.get("deliveryMode")) {
+            case "direct" :
+                delivery = new DirectDelivery();
+                break;
+            case "express" :
+                delivery = new ExpressDelivery(params.get("deliveryInfo"));
+                break;
+            case "relay" :
+                delivery = new RelayDelivery(Integer.parseInt(params.get("deliveryInfo")));
+                break;
+            case "takeAway" :
+                delivery = new TakeAwayDelivery();
+                break;
+        }
+        Bill bill = new Bill(customer, delivery);
+        String[] productsParams = params.get("products").split("%0D%0A");
+        for (String productLine : productsParams) {
+            String[] productAndQuantity = productLine.split("%3A");
+            Product product = products.get(Integer.parseInt(productAndQuantity[0]));
+            Integer quantity = Integer.parseInt(productAndQuantity[1]);
+            bill.addProduct(product, quantity);
+        }
+        bill.generate(new Writer() {
+            @Override
+            public void start() {
+            }
+            @Override
+            public void writeLine(String line) {
+                try {
+                    resp.getWriter().println("<br/>" + line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void stop() {
+            }
+        });
+    }
 
+
+    public Map<String, String> splitParameters(String queryString) {
+        String[] brutParams = queryString.split("&");
+        Map<String, String> params = new HashMap<>();
+        for (String brutParam : brutParams) {
+            String[] keyAndValue = brutParam.split("=");
+            if (keyAndValue.length == 2)
+                params.put(keyAndValue[0], keyAndValue[1]);
+        }
+        return params;
     }
 }
